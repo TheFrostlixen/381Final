2cd2dadfb779c5af64d0b180b57caffaaa6b4aa2
bigmacstorm <aaronbmwhitehead@gmail.com>
2015-04-15 12:59:45 -0700

    Made temp track

--- gameMgr.py
+++ gameMgr.py
@@ -27,7 +27,38 @@ class GameMgr:
         for z in range(0,10):
             obs = self.engine.entityMgr.createObs(pos = Vector3(positionTemp, 0, -200))
             obs = self.engine.entityMgr.createObs(pos = Vector3(positionTemp, 0, 600))
+            obs = self.engine.entityMgr.createObs(pos = Vector3(positionTemp, 0, -600))
+            obs = self.engine.entityMgr.createObs(pos = Vector3(positionTemp, 0, -1000))
             positionTemp += 100
+
+        #positionTemp = 0
+        #for z in range(0,5):
+
+        #create right bend
+        #interior
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1000, 0, -200))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1100, 0, -300))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1200, 0, -400))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1100, 0, -500))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1000, 0, -600))
+        #exterior
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1000, 0, 500))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1100, 0, 400))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1200, 0, 300))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1300, 0, 200))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1400, 0, 100))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1500, 0, 000))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1600, 0, -100))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1600, 0, -200))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1600, 0, -300))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1600, 0, -400))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1600, 0, -500))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1500, 0, -600))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1400, 0, -700))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1300, 0, -800))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1200, 0, -900))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1100, 0, -1000))
+        obs = self.engine.entityMgr.createObs(pos = Vector3(1000, 0, -1000))
 
 
     def tick(self, dt):
