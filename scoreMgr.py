2a24ac1c9dec7a96c64453e3d9fa54bf38d779e8
bigmacstorm <aaronbmwhitehead@gmail.com>
2015-05-03 20:06:28 -0700

    finished highscore

--- scoreMgr.py
+++ scoreMgr.py
@@ -8,18 +8,26 @@ import random
 class ScoreMgr:        
     def __init__(self, engine):        
         print "starting score Manger"        
-        self.engine = engine        
+        self.engine = engine
+        self.currentRun = 0        
                 
     def init(self): 
         self.scoreList = []
 
         with open('HighScores') as file:
+            line_value = file.readline(100).strip()
+            if len(line_value) > 0:
+                print line_value
+                line_value = int(line_value)
+            self.currentRun = int(line_value)
+            print self.currentRun
             for line in file:
-                self.scoreList.append(int(line))
-                print(int(line))
+                self.scoreList.append([int(x) for x in line.split()])
+            print(self.scoreList)
+            self.currentRun += 1
 
     def addCurrentTime(self):
-        self.scoreList.append(int(self.engine.overlayMgr.overlayList[1].curTime))
+        self.scoreList.append((self.currentRun, int(self.engine.overlayMgr.overlayList[1].curTime)))
         self.scoreList.sort()
             
     def tick(self, dt):   
@@ -27,8 +35,9 @@ class ScoreMgr:
                 
     def stop(self):        
         file = open('HighScores', 'w')
-        for x in self.scoreList:
-            file.write('%d \n' % x)
+        file.write('%d \n' % self.currentRun)
+        for (x, y) in self.scoreList:
+            file.write('%d %d \n' % (x,y))
         file.close
         
         
